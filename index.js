2b0abb99d080e4cc204f4b036c8ba0c165b4fa08
MrKai94 <srkai94@gmail.com>
2021-08-24 09:08:22 +0200

    FirstUpload

---
+++ index.js
@@ -0,0 +1,48 @@
+import { Client, Intents } from "discord.js";
+import axios from "axios";
+import express from "express";
+
+const server = express();
+
+server.get('/', function (req, res) {
+  res.send('BTCTracker')
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Server is ready.");
+  });
+}
+
+const url =
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+let lastPrice;
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+const BOT_ID = process.env["BOT_ID"];
+const BOT_TOKEN = process.env["BOT_TOKEN"];
+
+client.once("ready", () => {
+  console.log("We on . . .");
+
+  const SERVER_ID = process.env["SERVER_ID"];
+  const guild = client.guilds.cache.get(SERVER_ID);
+  const BOT = guild.members.cache.get(BOT_ID);
+
+  setInterval(async () => {
+    getPrice();
+    BOT.user.setActivity("BTC: " + lastPrice);
+  }, 5000);
+});
+
+function getPrice() {
+  axios.get(url).then((response) => {
+    lastPrice = response.data.ethereum.usd;
+  });
+}
+
+keepAlive();
+client.login(BOT_TOKEN);
